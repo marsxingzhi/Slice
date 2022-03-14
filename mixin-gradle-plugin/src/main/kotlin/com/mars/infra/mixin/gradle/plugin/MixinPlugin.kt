b27f439a1634d07565c1f5ff11e64e8d957ecda8
geyan <2277087813@qq.com>
2022-03-14 13:53:58 +0800

    init project

---
+++ mixin-gradle-plugin/src/main/kotlin/com/mars/infra/mixin/gradle/plugin/MixinPlugin.kt
@@ -0,0 +1,16 @@
+package com.mars.infra.mixin.gradle.plugin
+
+import com.android.build.gradle.AppExtension
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * Created by Mars on 2022/3/14
+ */
+class MixinPlugin: Plugin<Project> {
+
+    override fun apply(project: Project) {
+        println("MixinPlugin---😄")
+        val appExtension = project.extensions.getByType(AppExtension::class.java)
+    }
+}
